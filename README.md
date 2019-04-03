4c37b11af5d07a41402d1f62f8c2ad373ce089f7
vinsonJiang <1060312153@qq.com>
2019-04-03 21:16:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# spring-boot-jetty
+
+springboot整合jetty
---
+++ src/main/java/io/vinson/server/controller/HomeController.java
@@ -0,0 +1,12 @@
+package io.vinson.server.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @author: jiangweixin
+ * @date: 2019/4/3
+ */
+@RestController
+public class HomeController {
+}
