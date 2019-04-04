110e3c84151ec4890b80bf80524097ef2d0a518b
vinsonJiang <1060312153@qq.com>
2019-04-04 15:51:39 +0800

    springboot默认以Jetty作为内嵌服务器启动

--- src/main/java/io/vinson/server/controller/HomeController.java
+++ src/main/java/io/vinson/server/controller/HomeController.java
@@ -1,5 +1,7 @@
 package io.vinson.server.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
 }
